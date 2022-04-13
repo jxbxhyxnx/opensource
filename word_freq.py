60418d789d70dc1cbba1888a176175494f497f60
jxbxhyxnx <jbh010204@naver.com>
2022-04-14 00:15:40 +0900

    delete special characters

--- word_freq.py
+++ word_freq.py
@@ -9,3 +9,6 @@ param_num = int(sys.argv[2])
 with open(file_name, 'r') as file:
     temp = file.readlines() #한줄씩 자르기
 
+for i in temp: #각 줄의 index[0]부터 끝까지 돌리기 위해 for문 선언
+    delete_text = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ', i)
+    str = delete_text.split()
