0c0116b85a969b50294c8b47043f96dfd10992f5
jxbxhyxnx <jbh010204@naver.com>
2022-04-14 00:58:49 +0900

    count each word

--- word_freq.py
+++ word_freq.py
@@ -5,10 +5,15 @@ from collections import OrderedDict
 
 file_name = sys.argv[1]
 param_num = int(sys.argv[2])
+word_count = defaultdict(lambda: 0)
 
 with open(file_name, 'r') as file:
     temp = file.readlines() #한줄씩 자르기
 
+
 for i in temp: #각 줄의 index[0]부터 끝까지 돌리기 위해 for문 선언
     delete_text = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ', i)
     str = delete_text.split()
+    ct = 0
+    for j in str:
+        word_count[j] += 1
