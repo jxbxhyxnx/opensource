fa32b7026f7d8c8e6ef78354447f506c969320f3
jxbxhyxnx <jbh010204@naver.com>
2022-04-14 01:19:48 +0900

    change list name str -> other because list callable error when int to str

--- word_freq.py
+++ word_freq.py
@@ -13,20 +13,11 @@ with open(file_name, 'r') as file:
 
 for i in temp: #각 줄의 index[0]부터 끝까지 돌리기 위해 for문 선언
     delete_text = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ', i)
-    str = delete_text.split()
+    temp = delete_text.split()
     ct = 0
-    for j in str:
+    for j in temp:
         word_count[j] += 1
 
 count_sorted = OrderedDict(sorted(word_count.items(), key = lambda t : t[1], reverse = True))
-for a, b in count_sorted.items():
-    print(a, b)
-
-ct = 0
-print("---------------------")
-for k,v in count_sorted.items():
-    if(ct != param_num):
-        print(k, v)
-        ct += 1
-    else:
-        break
+#for a, b in count_sorted.items():
+ #   print(a, b)
